a6c3aa06b8c3d50e29014fc1f446c3132f39ec41
Максим Пушкарев <maks_pushkarev_2000@mail.ru>
2024-11-27 13:38:08 +0700

    добавлен метод для определения эффективности управляемых СРН

--- LibraryEnergizingPowerLine/EfficiencyMeansControlVoltage.cs
+++ LibraryEnergizingPowerLine/EfficiencyMeansControlVoltage.cs
@@ -1,17 +1,88 @@
 ﻿using ASTRALib;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace LibraryEnergizingPowerLine
 {
     public class EfficiencyMeansControlVoltage
     {
-        public static void CalculationEfficiencySwitchedMCV(IRastr rastr, List<Line> listLine)
+        /// <summary>
+        /// Метод для определения эффективности коммутируемых СРН.
+        /// </summary>
+        /// <param name="rastr">Экземпляр Rastr.</param>
+        /// <param name="listVoltageControlNode">Лист КП.</param>
+        /// <param name="nameVoltageControlNode">Название КП.</param>
+        /// <param name="mCV">Номер СРН.</param>
+        /// <returns>Строка со значениями эффективности СРН.</returns>
+        public static string CalculationEfficiencySwitchedMCV(IRastr rastr, List<VoltageLowerLimit> listVoltageControlNode,
+            string nameVoltageControlNode, int mCV)
         {
+            // Обращение к таблице Реакторы.
+            ITable tableReactor = (ITable)rastr.Tables.Item("Reactors");
+            // Обращение к колонке Состояние реактора.
+            ICol staReactor = (ICol)tableReactor.Cols.Item("sta");
+            // Обращение к колонке Название реактора.
+            ICol nameReactor = (ICol)tableReactor.Cols.Item("Name");
 
+            // Перебор строк в листе listVoltageControlNode и определение
+            // эффективности СРН в КП.
+            int nodeNumber = 0;
+            for (int i = 0; i < listVoltageControlNode.Count; i++)
+            {
+                if (listVoltageControlNode[i].Name == nameVoltageControlNode)
+                {
+                    nodeNumber = listVoltageControlNode[i].Number;
+                }
+            }
+
+            double U = GetVoltageNode(rastr, nodeNumber);
+
+            var setSelNodeR = "Id=" + mCV;
+            tableReactor.SetSel(setSelNodeR);
+            var indexReactor = tableReactor.FindNextSel[-1];
+            string nameR = (string)nameReactor.ZN[indexReactor];
+
+            while (indexReactor >= 0)
+            {
+                if ((bool)staReactor.Z[indexReactor] == false)
+                {
+                    return $"Реактор <<{nameR}>> уже в работе";
+                }
+
+                else if ((bool)staReactor.Z[indexReactor] == true)
+                {
+                    staReactor.Z[indexReactor] = 0;
+                    indexReactor = tableReactor.FindNextSel[indexReactor];
+                }
+                break;
+            }
+
+            rastr.rgm("");
+
+            double U1 = GetVoltageNode(rastr, nodeNumber);
+
+            double eFF = Math.Round(Math.Abs(U1 - U), 3);
+
+           //Если понадобится выполнять параллельный расчет, то освобождаем также
+           //в методе все таблицы и колонки - Marshal.FinalReleaseComObject(tableNode);
+
+            return $"Реактор - {nameR}\n" 
+                + $"Напряжение до - {U} кВ\n" 
+                + $"Напряжение после - {U1} кВ\n" 
+                + $"Эффективность СРН - {eFF}";
+        }
+
+        public static double GetVoltageNode(IRastr rastr, int nodeNumber)
+        {
+            // Обращение к таблице ветви.
+            ITable tableNode = (ITable)rastr.Tables.Item("node");
+            // Обращение к колонке Напряжение в узле.
+            ICol vrasNode = (ICol)tableNode.Cols.Item("vras");
+
+            var setSelNodeVras = "ny=" + nodeNumber;
+            tableNode.SetSel(setSelNodeVras);
+            var nodeNumberVras = tableNode.FindNextSel[-1];
+            double Unode = Math.Round((double)vrasNode.Z[nodeNumberVras], 3);
+            return Unode;
         }
     }
 }
--- Сalculation_U_dop/Program.cs
+++ Сalculation_U_dop/Program.cs
@@ -1,12 +1,14 @@
 ﻿using ASTRALib;
 using LibraryEnergizingPowerLine;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace Сalculation_U_dop
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             // Создаем экземпляр Rastr.
             IRastr rastr = new Rastr();
@@ -99,36 +101,161 @@ namespace ConsoleApp2
                 new VoltageLowerLimit(133, "ПС 500 кВ Иркутская", 494, 525)
             };
             
-            rastr.rgm("");
-            Console.WriteLine("Напряжение по концам ЛЭП в исходном режиме");
-            Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
+            //rastr.rgm("");
+            //Console.WriteLine("Напряжение по концам ЛЭП в исходном режиме");
+            //Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
             
-            // Одностороннее включение ЛЭП
-            EnergizingPowerLine.OneWayLineComutation(rastr, listLine, 551, 1, 1);
-            rastr.rgm("");
-            Console.WriteLine("Напряжение по концам ЛЭП при одностороннем включении");
-            Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
-            string patch1 =
-                @"C:\Users\maks_\OneDrive\Рабочий стол\УЧЁБА ТПУ\Магистратура\диплом\2024\СМЗУ\05022024_12_02_36\regim1";
-            rastr.Save(patch1, "");
+            //// Одностороннее включение ЛЭП
+            //EnergizingPowerLine.OneWayLineComutation(rastr, listLine, 551, 1, 1);
+            //rastr.rgm("");
+            //Console.WriteLine("Напряжение по концам ЛЭП при одностороннем включении");
+            //Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
+            //string patch1 =
+            //    @"C:\Users\maks_\OneDrive\Рабочий стол\УЧЁБА ТПУ\Магистратура\диплом\2024\СМЗУ\05022024_12_02_36\regim1";
+            //rastr.Save(patch1, "");
 
-            // Включение ЛЭП с двух сторон
-            EnergizingPowerLine.TwoWayLineComutation(rastr, listLine, 551, 1);
-            rastr.rgm("");
-            Console.WriteLine("Напряжение по концам ЛЭП при включении с двух сторон");
-            Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
-            string patch2 =
-                @"C:\Users\maks_\OneDrive\Рабочий стол\УЧЁБА ТПУ\Магистратура\диплом\2024\СМЗУ\05022024_12_02_36\regim2";
-            rastr.Save(patch2, "");
+            //// Включение ЛЭП с двух сторон
+            //EnergizingPowerLine.TwoWayLineComutation(rastr, listLine, 551, 1);
+            //rastr.rgm("");
+            //Console.WriteLine("Напряжение по концам ЛЭП при включении с двух сторон");
+            //Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
+            //string patch2 =
+            //    @"C:\Users\maks_\OneDrive\Рабочий стол\УЧЁБА ТПУ\Магистратура\диплом\2024\СМЗУ\05022024_12_02_36\regim2";
+            //rastr.Save(patch2, "");
 
-            // Отключение ЛЭП с двух сторон
-            EnergizingPowerLine.TwoWayLineAnComutation(rastr, listLine, 551, 0);
-            rastr.rgm("");
-            Console.WriteLine("Напряжение по концам ЛЭП при отключении с двух сторон");
-            Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
-            string patch3 =
-                @"C:\Users\maks_\OneDrive\Рабочий стол\УЧЁБА ТПУ\Магистратура\диплом\2024\СМЗУ\05022024_12_02_36\regim3";
-            rastr.Save(patch3, "");
+            //// Отключение ЛЭП с двух сторон
+            //EnergizingPowerLine.TwoWayLineAnComutation(rastr, listLine, 551, 0);
+            //rastr.rgm("");
+            //Console.WriteLine("Напряжение по концам ЛЭП при отключении с двух сторон");
+            //Console.WriteLine(EnergizingPowerLine.GetVoltageNode(rastr, listLine, "ВЛ 500 кВ Барнаульская - Рубцовская"));
+            //string patch3 =
+            //    @"C:\Users\maks_\OneDrive\Рабочий стол\УЧЁБА ТПУ\Магистратура\диплом\2024\СМЗУ\05022024_12_02_36\regim3";
+            //rastr.Save(patch3, "");
+
+            List<int> switchMCVRubtsovskaya = new List<int>()
+            {
+                // Р-1 ПС 500 кВ Барнаульская
+                9,
+                // Р-2 ПС 500 кВ Барнаульская
+                10,
+                // Р-3 ПС 500 кВ Барнаульская
+                11,
+                // Р-1 ПС 500 кВ Рубцовская
+                7,
+                // Р-2 ПС 500 кВ Рубцовская
+                8,
+                // Р-5537 ЕЭК
+                40,
+                // Р1-1106 ПС 1150 кВ Алтай
+                14,
+                // Р2-1106 ПС 1150 кВ Алтай
+                15,
+                // Р3-1104 ПС 1150 кВ Алтай
+                12,
+                // Р4-1104 ПС 1150 кВ Алтай
+                13,
+                // Р-Л-5370 ПС 500 кВ Семей
+                41,
+                // Р-Л-5384 ПС 500 кВ Семей
+                42,
+                // Р-5577 Экибастузская ГРЭС-1
+                44,
+                // БСК-1-220 ПС 220 кВ Светлая
+                125,
+                // БСК-2-220 ПС 220 кВ Светлая
+                126,
+            };
+
+            List<int> switchMCVBarnaulskaya = new List<int>()
+            {
+                // Р-1 ПС 500 кВ Барнаульская
+                9,
+                // Р-2 ПС 500 кВ Барнаульская
+                10,
+                // Р-3 ПС 500 кВ Барнаульская
+                11,
+                // Р1-1106 ПС 1150 кВ Алтай
+                14,
+                // Р2-1106 ПС 1150 кВ Алтай
+                15,
+                // Р3-1104 ПС 1150 кВ Алтай
+                12,
+                // Р4-1104 ПС 1150 кВ Алтай
+                13,
+                // Р-532 ПС 500 кВ Заря
+                6,
+                // Р-1 ПС 500 кВ Рубцовская
+                7,
+                // Р-2 ПС 500 кВ Рубцовская
+                8,
+                // Р-541 ПС 500 кВ Новокузнецкая
+                19,
+                // Р-542 ПС 500 кВ Новокузнецкая
+                20,
+                // БСК-1-220 ПС 220 кВ Светлая
+                125,
+                // БСК-2-220 ПС 220 кВ Светлая
+                126,
+                // ПС 1150 кВ Итатская 1РШ
+                23,
+                // ПС 1150 кВ Итатская 2РШ
+                24,
+                // ПС 1150 кВ Итатская 3РШ
+                25,
+                // ПС 1150 кВ Итатская 4РШ
+                26,
+                // ПС 1150 кВ Итатская 5РШ
+                27,
+                // Р-1 ПС 500 кВ Ново-Анжерская
+                16,
+                // Р-2 ПС 500 кВ Ново-Анжерская
+                17,
+                // Р-534 ПС 500 кВ Барабинская
+                5,
+                // ШР-500 ПС 500 кВ Томская
+                18,
+            };
+
+
+#if false
+            Console.WriteLine("КП ПС 500 кВ Рубцовская");
+
+            Parallel.ForEach<int>(
+                  switchMCVRubtsovskaya,
+                  Efficiency
+            );
+
+            void Efficiency(int MCV)
+            {
+                IRastr rastr = new Rastr();
+                rastr.Load(RG_KOD.RG_REPL, patch, "");
+                Console.WriteLine($"{EfficiencyMeansControlVoltage.CalculationEfficiencySwitchedMCV(
+                    rastr, listVoltageControlNode, "ПС 500 кВ Рубцовская", MCV)}");
+                Marshal.FinalReleaseComObject(rastr);
+            }
+#else
+            // Расчет эффективности коммутируемых СРН для КП ПС 500 кВ Рубцовская 
+            Console.WriteLine("КП ПС 500 кВ Рубцовская");
+
+            foreach (int MCV in switchMCVRubtsovskaya)
+            {
+                rastr.Load(RG_KOD.RG_REPL, patch, "");
+                Console.WriteLine($"{MCV}");
+                Console.WriteLine($"{EfficiencyMeansControlVoltage.CalculationEfficiencySwitchedMCV(
+                    rastr, listVoltageControlNode, "ПС 500 кВ Рубцовская", MCV)}");
+            }
+
+            // Расчет эффективности коммутируемых СРН для КП ПС 500 кВ Барнаульская
+            Console.WriteLine("КП ПС 500 кВ Барнаульская");
+
+            foreach (int MCV in switchMCVBarnaulskaya)
+            {
+                rastr.Load(RG_KOD.RG_REPL, patch, "");
+                Console.WriteLine($"{MCV}");
+                Console.WriteLine($"{EfficiencyMeansControlVoltage.CalculationEfficiencySwitchedMCV(
+                    rastr, listVoltageControlNode, "ПС 500 кВ Барнаульская", MCV)}");
+            }
+#endif
         }
     }
 }
